85b8c12e48a5ccbe29fa1e6dbec7317cd6ddb48b
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-09-24 18:16:22 -0500

    Changed template

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,53 +2,52 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  const faceio = new faceIO("fioa9d9e");
-  function signIn()
-  {
-     faceio.enroll({
-          "locale": "auto", 
-          "payload": {
-          "pin": 123456, 
-          "email": "john.doe@example.com"
-          }
-      }).then(userInfo => {
-          alert(
-              `User Successfully Enrolled! Details:
-              Unique Facial ID: ${userInfo.facialId}
-              Enrollment Date: ${userInfo.timestamp}
-              Gender: ${userInfo.details.gender}
-              Age Approximation: ${userInfo.details.age}`
-          );
-          console.log(userInfo);
-      }).catch(errCode => {
-          console.log(errCode);
-      })
-  }
-    <button onClick={signIn}>Sign-In</button>
+  let faceio;
 
-    const handleLogin = async () => {
-      try {
-          let result = await faceio.authenticate({
-          locale: "auto",
-          });
-          console.log(`Unique Facial ID: ${result.facialId}
-              Payload: ${result.payload}
-              `);
-      } catch (error) {
-          console.log(error);
-      }
+  useEffect(() => {
+    faceio = new faceIO("fioa414d");
+  }, []);
+
+  const handleSignIn = async () => {
+    try {
+      let response = await faceio.enroll({
+        locale: "auto",
+        payload: {
+          email: "example@gmail.com",
+          pin: "12345",
+        },
+      });
+
+      console.log(` Unique Facial ID: ${response.facialId}
+      Enrollment Date: ${response.timestamp}
+      Gender: ${response.details.gender}
+      Age Approximation: ${response.details.age}`);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
-  <button onClick = {handleLogin}>Log-In</button>
+
+  const handleLogIn = async () => {
+    try {
+      let response = await faceio.authenticate({
+        locale: "auto",
+      });
+
+      console.log(` Unique Facial ID: ${response.facialId}
+          PayLoad: ${response.payload}
+          `);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section>
       <h1>Face Authentication by FaceIO</h1>
-      <button onClick={signIn}>Sign-in</button>
-      <button onClick={handleLogin}>Log-in</button>
+      <button onClick={handleSignIn}>Sign-in</button>
+      <button onClick={handleLogIn}>Log-in</button>
     </section>
   );
-
 }
 
 export default App;
