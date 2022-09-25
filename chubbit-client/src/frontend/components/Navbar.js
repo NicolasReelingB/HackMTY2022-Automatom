d86a5cbe1e9393e32bc11f9cec7eb87ea79f9fbd
Do Hyun Nam <dollis2002@gmail.com>
2022-09-25 02:04:54 -0500

    asdada

--- chubbit-client/src/frontend/components/Navbar.js
+++ chubbit-client/src/frontend/components/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navigation = ({ web3Handler, account }) => {
   return (
-    <nav className="flex flex-row justify-between py-4 items-center bg-black sticky top-0 left-0 w-full h-18">
+    <nav className="flex flex-row justify-between py-4 items-center bg-black sticky top-0 left-0 w-full h-18 z-50">
       <div className="flex flex-row justify-center items-center w-1/4">
         <h1 className="text-3xl font-bold text-white logo-text">Chubbit</h1>
       </div>
