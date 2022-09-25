6036c4914410821ec319ef81a315e50874660218
NicolasRB <a01025335@itesm.mx>
2022-09-24 20:57:39 -0500

    React Routing Implemented

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import RouterNav from '../routes/RouterNav'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterNav/>
     <App />
   </React.StrictMode>
 )
